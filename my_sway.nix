3495807f33f8fc4c11deed1c96611748efe5b86a
Arthur Teisseire <arthurteisseire33@gmail.com>
2023-03-10 17:52:16 +0100

    Fix long waybar to load

--- my_sway.nix
+++ my_sway.nix
@@ -36,7 +36,7 @@
     extraSessionCommands = ''
       export SDL_VIDEODRIVER=wayland
       # needs qt5.qtwayland in systemPackages
-      export QT_QPA_PLATFORM=wayland
+      export QT_QPA_PLATFORM=wayland-egl
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
       # Fix for some Java AWT applications (e.g. Android Studio),
       # use this if they aren't displayed properly:
